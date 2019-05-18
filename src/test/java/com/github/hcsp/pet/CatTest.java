6b877a0992f2a5d724af972761f401846668fb34
Bo Zhang <bo@gradle.com>
2019-05-18 21:29:33 +0800

    Fix package name

---
+++ src/test/java/com/github/hcsp/pet/CatTest.java
@@ -0,0 +1,11 @@
+package com.github.hcsp.pet;
+
+import org.junit.jupiter.api.Test;
+
+public class CatTest {
+    @Test
+    public void catConstructorTest() {
+        new Cat("");
+        new Cat();
+    }
+}
