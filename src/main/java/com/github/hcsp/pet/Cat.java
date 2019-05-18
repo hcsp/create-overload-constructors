92b2f43874724b0c1cfe8b916c2306cd44a26f40
Bo Zhang <bo@gradle.com>
2019-05-18 20:19:56 +0800

    More repositories

---
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -0,0 +1,13 @@
+package com.github.hcsp.pet;
+
+public class Cat {
+    public String name;
+
+    // Create two constructor here: Cat() and Cat(String name)
+    // 在这里创建两个构造器：Cat()和Cat(String name)
+
+    @Override
+    public String toString() {
+        return "Cat(" + name + ")";
+    }
+}
---
+++ src/main/java/com/github/hcsp/pet/Home.java
@@ -0,0 +1,13 @@
+package com.github.hcsp.pet;
+
+public class Home {
+    public static void main(String[] args) {
+        // Create two constructor in Cat.java: Cat() and Cat(String name)
+        // 在Cat.java中创建两个构造器：Cat()和Cat(String name)
+        System.out.println(new Cat("Black"));
+
+        Cat white = new Cat();
+        white.name = "White";
+        System.out.println(white);
+    }
+}
