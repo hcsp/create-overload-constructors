aada96e4799cf032500c088994b052d7a455ea1d
chenxi1916 <60290522+chenxi1916@users.noreply.github.com>
2020-02-05 19:36:04 +0800

    创建两个构造器 (#153)
    创建两个构造器 (#153)

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -2,7 +2,12 @@ package com.github.hcsp.pet;
 
 public class Cat {
     public String name;
+    public Cat (){
 
+    }
+    public Cat (String name){
+
+    }
     // Create two constructor here: Cat() and Cat(String name)
     // 在这里创建两个构造器：Cat()和Cat(String name)
 
