3012fdaa921d71bc0509e1ee4eddb83aee1765a2
XinleZhou <59831330+XinleZhou@users.noreply.github.com>
2020-03-07 14:53:09 +0800

    pushee (#173)
    pushee (#173)

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -3,6 +3,14 @@ package com.github.hcsp.pet;
 public class Cat {
     public String name;
 
+    public Cat() {
+
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
     // Create two constructor here: Cat() and Cat(String name)
     // 在这里创建两个构造器：Cat()和Cat(String name)
 
