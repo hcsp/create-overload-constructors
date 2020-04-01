98f89734937f890084b659217429f703d0203978
yujie-zhang <907420318@qq.com>
2020-04-01 20:32:35 +0800

    创建两个构造器，加油! (#186)
    创建两个构造器，加油! (#186)

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -2,6 +2,15 @@ package com.github.hcsp.pet;
 
 public class Cat {
     public String name;
+    public Cat() {
+
+    }
+
+
+    public Cat(String b) {
+        this.name = b;
+    }
+
 
     // Create two constructor here: Cat() and Cat(String name)
     // 在这里创建两个构造器：Cat()和Cat(String name)
