a7238afad987a1156e9ad19878ebdcb43e21b5ae
neverland <zink_zzz@163.com>
2020-08-09 22:42:22 +0800

    create two constructor (#223)
    create two constructor (#223)

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -1,13 +1,19 @@
 package com.github.hcsp.pet;
 
 public class Cat {
-    public String name;
+  public String name;
 
-    // Create two constructor here: Cat() and Cat(String name)
-    // 在这里创建两个构造器：Cat()和Cat(String name)
+  // Create two constructor here: Cat() and Cat(String name)
+  // 在这里创建两个构造器：Cat()和Cat(String name)
+  public Cat() {
+  }
 
-    @Override
-    public String toString() {
-        return "Cat(" + name + ")";
-    }
+  public Cat(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return "Cat(" + name + ")";
+  }
 }
