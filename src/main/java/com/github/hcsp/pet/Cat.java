6380767da7fdad93f17ad5b1f933c225b876964d
xu-kai-quan <50357378+xu-kai-quan@users.noreply.github.com>
2020-12-05 21:30:30 +0800

    代码规范

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -1,15 +1,18 @@
 package com.github.hcsp.pet;
 
 public class Cat {
+    
     public String name;
 
     // Create two constructor here: Cat() and Cat(String name)
+    
     // 在这里创建两个构造器：Cat()和Cat(String name)
+    
     public  Cat() {
 
     }
     
-    public Cat(String name) {
+    public Cat( String name ) {
         this.name = name;
     }
     
