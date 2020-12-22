c71bd67951406ccb9ff4e213fc411efa41103218
jumodada <864353360@qq.com>
2020-12-22 13:12:12 +0800

    Update Cat.java (#245)
    Update Cat.java (#245)

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -6,6 +6,14 @@ public class Cat {
     // Create two constructor here: Cat() and Cat(String name)
     // 在这里创建两个构造器：Cat()和Cat(String name)
 
+    public Cat() {
+        
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
         return "Cat(" + name + ")";
