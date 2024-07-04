7d4cdabd14da8a165f355e3b90691ebf0e75b986
buuoltwo <57209714+buuoltwo@users.noreply.github.com>
2024-07-04 23:20:00 +0800

    1 (#324)
    1 (#324)

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -5,7 +5,10 @@ public class Cat {
 
     // Create two constructor here: Cat() and Cat(String name)
     // 在这里创建两个构造器：Cat()和Cat(String name)
-
+    public Cat() {}
+    public Cat(String s) {
+        this.name = s;
+    }
     @Override
     public String toString() {
         return "Cat(" + name + ")";
